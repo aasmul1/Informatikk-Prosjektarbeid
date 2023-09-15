de1a0f89f1d2f3a246fdae3a9759a21a02109669
olavhalleraker <ohallera@gmail.com>
2023-09-15 10:39:27 +0200

    NotesStorage created

---
+++ notes/core/src/main/java/core/NotesStorage.java
@@ -0,0 +1,16 @@
+package core;
+
+import java.io.File;
+
+public class NotesStorage {
+    private File file;
+    
+    public NoteOverview readNoteOverview() {
+        //TODO
+    return null;
+    }
+
+    public void writeNoteOverview(NoteOverview noteOverview) {
+        //TODO
+    }
+}
