822a75b0ba4d88ed0dcda3b1443869b5f5f1192d
Hedda Jendem <heddajen@ntnu.no>
2023-09-25 14:04:23 +0200

    Created controller and fxml-file for new edit note scene

---
+++ notes/ui/src/main/java/ui/NoteEditController.java
@@ -0,0 +1,25 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class NoteEditController {
+
+    @FXML
+    private AnchorPane noteeditpane; 
+
+    @FXML
+    private TextField newNoteInputTitle; 
+
+    @FXML
+    private Button addNewNote;
+
+    @FXML
+    private TextArea newNoteInputText;
+
+
+    
+}
