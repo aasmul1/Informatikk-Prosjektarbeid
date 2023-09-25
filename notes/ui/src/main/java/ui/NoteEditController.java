34420058d032fb8ba1e2cbbeace1bedf8ba65a6c
Hedda Jendem <heddajen@ntnu.no>
2023-09-25 15:36:53 +0200

    Logic for scene change  in NoteEditController

--- notes/ui/src/main/java/ui/NoteEditController.java
+++ notes/ui/src/main/java/ui/NoteEditController.java
@@ -1,10 +1,17 @@
 package ui;
 
+import java.io.IOException;
+
+import core.Note;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class NoteEditController {
 
@@ -19,6 +26,26 @@ public class NoteEditController {
 
     @FXML
     private TextArea newNoteInputText;
+
+
+
+    public void sendToAppScene(Note newnote) throws IOException{
+
+        Stage currentStage = (Stage) noteeditpane.getScene().getWindow();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/App.fxml"));
+        Parent root = loader.load();
+
+        AppController appController = loader.getController();
+        appController.updateinfo(newnote);
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("NoteBook Overview");
+        stage.show();
+
+        currentStage.close();    
+    }
 
 
     
