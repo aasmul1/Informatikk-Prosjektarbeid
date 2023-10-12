b7f8535da6effcc1df50f349c2d7b2539bfabfc6
olavhalleraker <ohallera@gmail.com>
2023-10-12 08:52:12 +0200

    Fixed such that LocalDate is added

--- notes/core/src/test/java/json/internal/NoteOverviewJsonTest.java
+++ notes/core/src/test/java/json/internal/NoteOverviewJsonTest.java
@@ -2,6 +2,7 @@ package json.internal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
@@ -17,28 +18,33 @@ public class NoteOverviewJsonTest {
         objectMapper.registerModule(new NoteOverviewModule());
     }
 
-    protected void assertEqualsIgnoreWhitespace(final String expected, final String actual) throws Exception {
+    private void assertEqualsIgnoreWhitespace(final String expected, final String actual) throws Exception {
         assertEquals(expected, actual.replaceAll("\\s+", ""));
 }
 
     @Test
     public void testNoteOverviewSerialization() throws Exception {
+        LocalDate LocalDate = java.time.LocalDate.of(2022, 1, 1);
+
         final String actualJson = objectMapper
-                .writeValueAsString(new NoteOverview(Arrays.asList(new Note("title", "text"), new Note("title2", "text2")) ));
+                .writeValueAsString(new NoteOverview(Arrays.asList(new Note("title", "text", LocalDate, LocalDate), new Note("title2", "text2", LocalDate, LocalDate)) ));
         final String expectedJson =
-                "[{\"title\":\"title\",\"text\":\"text\"},{\"title\":\"title2\",\"text\":\"text2\"}]";
+                "[{\"title\":\"title\",\"text\":\"text\",\"created\":\"2022-01-01\",\"edited\":\"2022-01-01\"},{\"title\":\"title2\",\"text\":\"text2\",\"created\":\"2022-01-01\",\"edited\":\"2022-01-01\"}]";
         assertEqualsIgnoreWhitespace(expectedJson, actualJson);
     }
 
     @Test
     public void testNoteOverviewDeserialization() throws Exception {
-        final String json = "[{\"title\":\"title\",\"text\":\"text\"},{\"title\":\"title2\",\"text\":\"text2\"}]";
+        final String json = 
+                "[{\"title\":\"title\",\"text\":\"text\",\"created\":\"2022-01-01\",\"edited\":\"2022-01-01\"},{\"title\":\"title2\",\"text\":\"text2\",\"created\":\"2022-01-01\",\"edited\":\"2022-01-01\"}]";
         final NoteOverview noteOverview = objectMapper.readValue(json, NoteOverview.class);
         assertEquals(2, noteOverview.getNotes().size());
         assertEquals("title", noteOverview.getNotes().get(0).getTitle());
         assertEquals("text", noteOverview.getNotes().get(0).getText());
         assertEquals("title2", noteOverview.getNotes().get(1).getTitle());
         assertEquals("text2", noteOverview.getNotes().get(1).getText());
+        assertEquals(LocalDate.of(2022, 1, 1), noteOverview.getNotes().get(0).getCreatedDate());
+        assertEquals(LocalDate.of(2022, 1, 1), noteOverview.getNotes().get(0).getEditedDate());
 
     }
 
