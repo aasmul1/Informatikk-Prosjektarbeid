3a7657ee1d0b4cce3605b01c6a6a275f58961c91
olavhalleraker <ohallera@gmail.com>
2023-10-12 08:33:54 +0200

    Test for Note scene (not done)
    Co-authored-by: Åsmund Løvoll <aasmul@stud.ntnu.no>

---
+++ notes/ui/src/test/java/ui/NoteTest.java
@@ -0,0 +1,61 @@
+package ui;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.matcher.control.LabeledMatchers;
+import org.testfx.matcher.control.TextInputControlMatchers;
+import static org.testfx.matcher.base.NodeMatchers.*;
+import org.testfx.matcher.control.LabeledMatchers;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationTest;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.base.NodeMatchers.*;
+
+
+/**
+ * TestFX Note test
+ */
+public class NoteTest extends ApplicationTest {
+
+    private NoteController controller;
+    private Parent root;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("Note.fxml"));
+        root = fxmlLoader.load();
+        controller = fxmlLoader.getController();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    // @Test
+    // public void testController_initial() {
+    //   assertNotNull(this.controller);
+    // }
+  
+    private void click(String label) {
+        clickOn(LabeledMatchers.hasText(label));
+    }
+
+    @Test
+    public void testNewNote() throws IOException {
+ 
+        clickOn("#newNoteInputTitle").write("Title");
+        clickOn("#newNoteInputText").write("Text");
+
+        verifyThat("Title", isVisible());
+        verifyThat("Text", isVisible());
+        // verifyThat("#newNoteInputText", hasText("Text"));
+    }  
+}
+
