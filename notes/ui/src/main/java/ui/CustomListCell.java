e22856632a3099bc43472eb877ed36a12c2ccb6a
aasmul <aasmul@stud.ntno.no>
2023-10-21 00:07:53 +0200

    Fixed how date, text and title is portrayed in App.fxml-mI set title in bold, set maximum wrapping width for title and text, and also fixed that title and text is always to the left, and date always to the right

--- notes/ui/src/main/java/ui/CustomListCell.java
+++ notes/ui/src/main/java/ui/CustomListCell.java
@@ -1,9 +1,13 @@
 package ui;
 
 import core.Note;
+import javafx.geometry.Pos;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
 public class CustomListCell extends ListCell<Note>{
@@ -15,8 +19,15 @@ public class CustomListCell extends ListCell<Note>{
     public CustomListCell() {
         super();
         VBox vBox = new VBox(title, text);
-        content = new HBox(date, vBox);
+        HBox hBox = new HBox(date);
+        
+        content = new HBox(vBox, hBox);
         content.setSpacing(10);
+        
+
+        // Set alignment for the child HBoxes
+        HBox.setHgrow(vBox, Priority.ALWAYS); // Makes vBox take as much space as possible on the left
+        HBox.setHgrow(hBox, Priority.NEVER);
     }
 
 	@Override
@@ -25,7 +36,10 @@ public class CustomListCell extends ListCell<Note>{
             if (note != null && !empty) { // <== test for null item and empty parameter
                 title.setText(note.getTitle());
                 text.setText(String.format("%s", note.getText()));
-				date.setText(String.format("%s",note.getCreatedDate()));
+                title.setFont(Font.font("Arial", FontWeight.BOLD, 13));
+				date.setText(String.format("%s", note.getCreatedDate()));
+                title.setWrappingWidth(300); // Set the maximum width for title text
+                text.setWrappingWidth(300);
                 setGraphic(content);
             } else {
                 setGraphic(null);
