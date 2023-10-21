0dcf5f246b94e3c2af79fe1745161dae8ca8c1f5
Hedda Jendem <heddajen@ntnu.no>
2023-10-21 10:43:23 +0200

    Created user interface in login scene and FXML-fields in LoginController

--- notes/ui/src/main/java/ui/LoginController.java
+++ notes/ui/src/main/java/ui/LoginController.java
@@ -1,5 +1,15 @@
 package ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+  @FXML
+  private TextField username, password; 
+
+  @FXML
+  private Button loginButton; 
   
 }
