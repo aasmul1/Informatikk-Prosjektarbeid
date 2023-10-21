d430cd48506c525db69a23bf319be1c53af50ae7
olavhalleraker <ohallera@gmail.com>
2023-10-21 13:41:49 +0200

    Module-info in Rest module
    - Added requirements to module info

--- notes/rest/src/main/java/module-info.java
+++ notes/rest/src/main/java/module-info.java
@@ -1,3 +1,19 @@
-module name {
-  
+module notes.rest {
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.beans;
+    requires spring.core;
+    requires spring.context;
+    requires spring.data.rest.core;
+    requires spring.data.commons;
+    requires spring.web;
+    requires spring.webmvc;
+
+    requires transitive notes.core;
+    // requires transitive notes.json;
+    requires transitive com.fasterxml.jackson.databind;
+
+    opens notes.rest to spring.beans, spring.context, spring.web, spring.core;
+    // exports notes.rest.properties to spring.beans, spring.boot;
+    exports notes.rest;
 }
