d6e432f87f7d5b4e1ad6892f92e817ba5f567f5c
Hedda Jendem <heddajen@ntnu.no>
2023-10-23 13:25:47 +0200

    addNote and getAllNotes methode in NotesService

--- notes/rest/src/main/java/rest/NotesService.java
+++ notes/rest/src/main/java/rest/NotesService.java
@@ -1,12 +1,25 @@
 package rest;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import core.Note;
 import core.NoteOverview;
 
 @Service
 public class NotesService {
 
     private NoteOverview noteOverview;
+
+    public void addNote(Note note) {
+        noteOverview.addNote(note);
+    }
+
+    public List<Note> getAllNotes() {
+        return noteOverview.getNotes();
+    }
+
+
     
 }
