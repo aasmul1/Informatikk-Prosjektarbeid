a3dafcf335c63095da82f3457a01ec1cf9bf26ad
Hedda Jendem <heddajen@ntnu.no>
2023-10-23 11:05:15 +0200

    Marked NoteController as a web controller
    - annotation @RestController and @RequestMapping()
    - javadoc

--- notes/rest/src/main/java/rest/NotesController.java
+++ notes/rest/src/main/java/rest/NotesController.java
@@ -1,9 +1,22 @@
 package rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @RestController marks the class as a web controller, capable of handling HTTP requests in a RESTful web service, such as JSON
+ * @RequestMapping(NotesController.NOTES_SERVICE_PATH) sets the base URI for all endpoints handled by this controller
+ * 
+ * This means that all the mappings defined in this controller will start with /noteOverveiw in their URI
+ * 
+ */
+@RestController 
+@RequestMapping(NotesController.NOTES_SERVICE_PATH) 
 public class NotesController {
 
-    //localhost:8080//notes
-  public static final String NOTES_SERVICE_PATH = "noteOverveiw";
+    
+    //localhost:8080//noteOverveiw 
+    public static final String NOTES_SERVICE_PATH = "noteOverveiw";
 
 
     
