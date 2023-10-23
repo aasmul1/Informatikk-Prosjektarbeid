fe5bbc20e8915be083597b55ba1e44fbc710f8f7
Hedda Jendem <heddajen@ntnu.no>
2023-10-23 12:25:52 +0200

    Created NoteService class
    - works as a service layer component in our application
    - new NoteOverview objekt

---
+++ notes/rest/src/main/java/rest/NotesService.java
@@ -0,0 +1,12 @@
+package rest;
+
+import org.springframework.stereotype.Service;
+
+import core.NoteOverview;
+
+@Service
+public class NotesService {
+
+    private NoteOverview noteOverview;
+    
+}
