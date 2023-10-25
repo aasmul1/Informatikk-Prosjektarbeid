3c90aa8b573dcc6943255956bbca7157c5e2adf8
Ida Tomine Berg <idatomine.berg@gmail.com>
2023-10-25 18:39:01 +0200

    LoginController.java
    Imported TextArea javafx
    
    Changed type of errorMessage field from Text to TextArea.
    
    Added method createUserActionButton, which is what happens when you press the create user button. Logic not implemented yet.
    
    Added method loadLoginInfo, but logic not implemented yet.
    
    Co-authored-by: Åsmund Løvoll <aasmul@stud.ntnu.no>

--- notes/ui/src/main/java/ui/controllers/LoginController.java
+++ notes/ui/src/main/java/ui/controllers/LoginController.java
@@ -6,6 +6,7 @@ import core.UserValidation;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
@@ -15,21 +16,28 @@ public class LoginController extends AbstractController{
    * Input fields
    */
   @FXML
-  private TextField usernameinput, passwordInput; 
+  private TextField usernameInput, passwordInput; 
 
   @FXML 
-  private Text errorMessage;
+  private TextArea errorMessage;
 
   /**
    * Button to initialize login
    */
   @FXML
   private Button loginButton; 
+
+  @FXML private Button createUserButton;
+
+  @FXML
+  public void createUserActionButton(ActionEvent event) throws IOException{
+    
+  }
   
   @FXML
   public void loginUser(ActionEvent event) throws IOException{
 
-    String username = usernameinput.getText();
+    String username = usernameInput.getText();
     String password = passwordInput.getText();
     try{
     UserValidation.checkValidUsername(username);
@@ -42,6 +50,10 @@ public class LoginController extends AbstractController{
     }catch(IllegalAccessError e){
         errorMessage.setText(e.getMessage());
     }
+  }
+
+  public void loadLoginInfo(){
+
   }
 
 }
