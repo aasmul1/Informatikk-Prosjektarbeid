74ab448a87f9d3b6f7dbe89ff72f39c387a8530f
Ida Tomine Berg <idatomine.berg@gmail.com>
2023-10-25 18:30:44 +0200

    RemoteNotesAccess.java
    Created class RemoteNotesAccess which implementes NotesAccess interface.
    
    Logic is not implemented yet.

---
+++ notes/ui/src/main/java/dataaccess/RemoteNotesAccess.java
@@ -0,0 +1,13 @@
+package dataaccess;
+
+import core.User;
+
+public class RemoteNotesAccess implements NotesAccess{
+
+    @Override
+    public void createUser(User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+    }
+  
+}
