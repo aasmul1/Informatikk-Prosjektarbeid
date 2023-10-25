fa13b9d466b30ce7c87d7240259fa0ad76361af2
Ida Tomine Berg <idatomine.berg@gmail.com>
2023-10-25 18:14:04 +0200

    LocalNotesAccess.java
    created class LocalNotesAccess which implements NotesAccess interface.
    
    Logic is not implemented yet

---
+++ notes/ui/src/main/java/dataaccess/LocalNotesAccess.java
@@ -0,0 +1,15 @@
+package dataaccess;
+
+import core.User;
+
+public class LocalNotesAccess implements NotesAccess{
+
+    @Override
+    public void createUser(User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+    }
+
+
+    
+}
