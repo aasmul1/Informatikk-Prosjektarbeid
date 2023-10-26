842bf1ee322be71686fb80b6423c699f1a1ceec3
Ida Tomine Berg <idatomine.berg@gmail.com>
2023-10-26 08:31:34 +0200

    CreateUserController.java
    Changed name on method from createUserAction to createUser.
    
    Changed the order on the UserValidation fields in the method to encapsulate better.
    
    Added some more logic, the method creates a user-object and an empty noteOverview object for the new user.
    
    setScene-method will be called when pressing create user button, and the user will be sent back to login-scene.
    
    Also added the methos loadCreateUserInfo, but have not implemented any logic here yet.
    
    CreateUserController class extends AbstractController now.
    
    Co-authored-by: Åsmund Løvoll <aasmul@stud.ntnu.no>

--- notes/ui/src/main/java/ui/controllers/CreateUserController.java
+++ notes/ui/src/main/java/ui/controllers/CreateUserController.java
@@ -8,13 +8,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import core.UserValidation;
+import core.NoteOverview;
 import core.User;
 
 /**
  * This is the class for controlling CreateUser scene.
  */
 
-public class CreateUserController {
+public class CreateUserController extends AbstractController{
     
     
     @FXML private TextField inputPassword;
@@ -28,28 +29,30 @@ public class CreateUserController {
     private String confirmPassword;
 
     @FXML
-    private void createUserAction(ActionEvent event) throws IOException {
+    private void createUser(ActionEvent event) throws IOException {
         
         try {
-
+            UserValidation.allFieldsEmpty(inputUsername.getText(), inputPassword.getText());
+            UserValidation.checkValidUsername(inputUsername.getText());
+            UserValidation.checkValidPassword(inputPassword.getText());
+            confirmPassword = inputConfirmPassword.getText();
+            UserValidation.checkEqualPassword(password, confirmPassword);
             username = inputUsername.getText();
             password = inputPassword.getText();
-            confirmPassword = inputConfirmPassword.getText();
-            UserValidation.allFieldsEmpty(username, password);
-            
-            UserValidation.checkValidUsername(username);
-            UserValidation.checkValidPassword(password);
-            UserValidation.checkEqualPassword(password, confirmPassword);
-            User ny = new User(username, password, null);
+
+            NoteOverview noteOverview = new NoteOverview(); //empty noteoverview
+            User user = new User(username, password, noteOverview);
+
+            getDataAccess().createUser(user);
+
+            setScene(Controllers.LOGIN, event, dataAccess, null);
+
         } catch (IllegalArgumentException e) {
             errorMessageDisplay.setText(e.getMessage());
         }
-
-
-
-
     }
 
+    public void loadCreateUserInfo(){
 
-
+    }
 }
