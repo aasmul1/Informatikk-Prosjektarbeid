cae5dd410ea10efcab9a31385e310534d847a98d
aasmul <aasmul@stud.ntno.no>
2023-10-26 09:03:02 +0200

    Removed import javafx.scene.text.Text

--- notes/ui/src/main/java/ui/controllers/LoginController.java
+++ notes/ui/src/main/java/ui/controllers/LoginController.java
@@ -8,7 +8,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 
 public class LoginController extends AbstractController{
 
