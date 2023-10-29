f661645526bee262ab9f6d10b251d02bb1e9c96f
aasmul <aasmul@stud.ntno.no>
2023-10-29 17:22:58 +0100

    Imported all methods from interface.
    Need logic in them.

--- notes/ui/src/main/java/dataaccess/LocalNotesAccess.java
+++ notes/ui/src/main/java/dataaccess/LocalNotesAccess.java
@@ -1,5 +1,11 @@
 package dataaccess;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import core.Accounts;
+import core.Note;
 import core.User;
 
 public class LocalNotesAccess implements NotesAccess{
@@ -8,6 +14,54 @@ public class LocalNotesAccess implements NotesAccess{
     public void createUser(User user) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+    }
+
+    @Override
+    public Accounts readAccounts() throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'readAccounts'");
+    }
+
+    @Override
+    public User readUser(String username) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'readUser'");
+    }
+
+    @Override
+    public User userLogin(String username, String password) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'userLogin'");
+    }
+
+    @Override
+    public void registerNewAccounts(Accounts accounts) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'registerNewAccounts'");
+    }
+
+    @Override
+    public void deleteAccounts() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAccounts'");
+    }
+
+    @Override
+    public void uploadFile(File file) throws IOException, InterruptedException, URISyntaxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'uploadFile'");
+    }
+
+    @Override
+    public Note getUserNote(String title, String text) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUserNote'");
+    }
+
+    @Override
+    public User getLoggedInUser() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getLoggedInUser'");
     }
 
 
