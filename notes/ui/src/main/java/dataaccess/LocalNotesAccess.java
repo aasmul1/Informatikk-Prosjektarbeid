40cb06a03d34267e3faf50e04068dfe086927fc7
aasmul <aasmul@stud.ntno.no>
2023-10-29 18:39:32 +0100

    Added methods and logic
    - Added getUserNote to get note from file
    - Added userLogin to get user from accounts with username and password
    - Added readUser with just username to get user
    - Added readAccounts to get the accounts
    - Added reading from file in constructor
    - Added createUser so a user can be created
    - Added getLoggedInUser to get the logged in user

--- notes/ui/src/main/java/dataaccess/LocalNotesAccess.java
+++ notes/ui/src/main/java/dataaccess/LocalNotesAccess.java
@@ -7,44 +7,66 @@ import java.net.URISyntaxException;
 import core.Accounts;
 import core.Note;
 import core.User;
+import json.NotesPersistence;
 
 public class LocalNotesAccess implements NotesAccess{
 
+    private Accounts accounts;
+    private User user = new User(null, null, null);
+    private File test;
+    private final NotesPersistence persistence = new NotesPersistence(test);
+
+    /**
+   * Loads accounts from json-file.
+   */
+    public LocalNotesAccess(){
+        // persistence.setFilePath("Accounts.json");
+        try {
+            // this.accounts = persistence.initializeStorage();
+        } catch (IllegalStateException e) {
+            this.accounts = new Accounts();
+            try {
+                // persistence.saveAccounts(accounts);
+            } catch (IllegalStateException e1) {
+                System.out.println(e1.getMessage());
+        }
+    }
+    }
+    
+    
+
     @Override
     public void createUser(User user) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+        if (user != null) {
+            accounts.addUser(user);
+          }
+          try {
+            // persistence.saveAccounts(accounts);
+          } catch (IllegalStateException e) {
+            e.printStackTrace();
+          }
     }
 
     @Override
     public Accounts readAccounts() throws IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'readAccounts'");
+        persistence.readNoteOverview();
+        return accounts; // må fikse
     }
 
     @Override
     public User readUser(String username) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'readUser'");
+        if (accounts.getUser(username) instanceof User) {
+            return (User) accounts.getUser(username);
+          }
+          return  null;
     }
 
     @Override
     public User userLogin(String username, String password) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'userLogin'");
+        return accounts.getUser(username, password);
+        
     }
 
-    @Override
-    public void registerNewAccounts(Accounts accounts) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'registerNewAccounts'");
-    }
-
-    @Override
-    public void deleteAccounts() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteAccounts'");
-    }
 
     @Override
     public void uploadFile(File file) throws IOException, InterruptedException, URISyntaxException {
@@ -53,16 +75,24 @@ public class LocalNotesAccess implements NotesAccess{
     }
 
     @Override
-    public Note getUserNote(String title, String text) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getUserNote'");
+    public Note getUserNote(String title, String username) {
+        User user = accounts.getUser(username);
+        for (Note note : user.getNoteOverview()) {
+            if(note.getTitle().equals(title)){
+                return note;
+            }
+        }
+        return null;
     }
 
     @Override
     public User getLoggedInUser() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getLoggedInUser'");
+        return this.user;
     }
+
+
+
+    
 
 
     
