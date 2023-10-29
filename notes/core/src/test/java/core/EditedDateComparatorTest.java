b69f21b0d9bf7b5a4d6ac8a71593cf2794aa7a20
Hedda Jendem <heddajen@ntnu.no>
2023-10-29 17:45:34 +0100

    Add Unit Tests for CreatedDateComparator
    - LocalDate variables
    - earlierNote and laterNote objects
    - instance of editDateConparator

--- notes/core/src/test/java/core/EditedDateComparatorTest.java
+++ notes/core/src/test/java/core/EditedDateComparatorTest.java
@@ -1,6 +1,22 @@
 package core;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
 public class EditedDateComparatorTest {
     
-    
+     LocalDate today = LocalDate.now();
+    LocalDate yesterday = today.minusDays(1);
+    LocalDate dayBeforeYesterday = today.minusDays(2);
+
+    Note earlierNote = new Note("Earlier Note", "This note was edited earlier.", dayBeforeYesterday, yesterday);
+    Note laterNote = new Note("Later Note", "This note was edited today.", dayBeforeYesterday, today);
+
+    // Create an instance of your comparator
+    EditedDateComparator comparator = new EditedDateComparator();
+
 }
