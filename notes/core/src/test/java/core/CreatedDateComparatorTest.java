b83376e7ee0ca25bc0076c7e7362e46adef183e4
Hedda Jendem <heddajen@ntnu.no>
2023-10-29 18:33:36 +0100

    Objects in createdDateComparatorTest
    - same as in editedDateComparator test
    - LocalDate variabler
    - earlierCreatedNote, laterCreatedNote objects
    comparator instance

--- notes/core/src/test/java/core/CreatedDateComparatorTest.java
+++ notes/core/src/test/java/core/CreatedDateComparatorTest.java
@@ -1,6 +1,19 @@
 package core;
 
+import java.time.LocalDate;
+
 public class CreatedDateComparatorTest {
+
+    LocalDate today = LocalDate.now();
+    LocalDate yesterday = today.minusDays(1);
+    LocalDate dayBeforeYesterday = today.minusDays(2);
+
+    Note earlierCreatedNote = new Note("Earlier Created Note", "This note was created earlier.", dayBeforeYesterday, yesterday);
+    Note laterCreatedNote = new Note("Later Created Note", "This note was created today.", yesterday, today);
+
+    // Create an instance of your comparator
+    CreatedDateComparator comparator = new CreatedDateComparator();
+    
 
 
 }
