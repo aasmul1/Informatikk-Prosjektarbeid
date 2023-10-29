9e3f2988a1ec4136646b768e453bedebbeb2b9e6
Hedda Jendem <heddajen@ntnu.no>
2023-10-29 18:54:44 +0100

    Note and comparator objects in TitleComparatorTest
    - noteA, noteB, noteC
    - TitleComparator comparator
    - BeforeEach setUp methode to initialize objects

--- notes/core/src/test/java/core/TitleComparatorTest.java
+++ notes/core/src/test/java/core/TitleComparatorTest.java
@@ -1,5 +1,23 @@
 package core;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class TitleComparatorTest {
+
+    private Note noteA;
+    private Note noteB;
+    private Note noteC;
+
+    // Create an instance of your comparator
+    private TitleComparator comparator;
+
+    @BeforeEach
+    public void setUp() {
+        noteA = new Note("A", "Text to A");
+        noteB = new Note("B", "Text to B");
+        noteC = new Note("C", "Text to C");
+
+        comparator = new TitleComparator();
+    }
     
 }
