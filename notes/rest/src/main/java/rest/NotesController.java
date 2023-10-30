e5f73deda7ec8a08ea70e6091eacbcb82a0090cf
olavhalleraker <ohallera@gmail.com>
2023-10-30 13:30:25 +0100

    HTTP GET user with login info
    - Added HTTP GET request with parameters username and password
    - notesService getUserByUsername is called
    - and then  validLogin method in accounts is called

--- notes/rest/src/main/java/rest/NotesController.java
+++ notes/rest/src/main/java/rest/NotesController.java
@@ -1,22 +1,50 @@
 package rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import core.Accounts;
+import core.NoteOverview;
+import core.User;
 
 /**
  * @RestController marks the class as a web controller, capable of handling HTTP requests in a RESTful web service, such as JSON
  * @RequestMapping(NotesController.NOTES_SERVICE_PATH) sets the base URI for all endpoints handled by this controller
  * 
- * This means that all the mappings defined in this controller will start with /noteOverveiw in their URI
+ * This means that all the mappings defined in this controller will start with /notes in their URI
  * 
  */
 @RestController 
 @RequestMapping(NotesController.NOTES_SERVICE_PATH) 
 public class NotesController {
 
-    
-    //localhost:8080//noteOverveiw 
-    public static final String NOTES_SERVICE_PATH = "noteOverveiw";
+    public static final String NOTES_SERVICE_PATH = "notes";  
+    private final NotesService notesService;  
+
+    @Autowired
+    public NotesController(final NotesService notesService) {
+        this.notesService = notesService;
+    }  
+
+    // localhost:8080//notes/login?username={username}&password={password}
+    @GetMapping(path = "login")
+    public User userLogin(@RequestParam("username") String username, @RequestParam("password") String password) {
+        if (notesService.userLogin(username, password)) {
+            return notesService.getUserByUsername(username);
+            }
+        else {
+            return null;
+        }
+    } 
+  
+
+    @GetMapping
+    public Accounts getAccounts() {
+        return notesService.getAccounts();
+    }  
 
 
     
