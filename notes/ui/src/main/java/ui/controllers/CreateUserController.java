ec6c0397de5ae16755d0e594e1c0d70d3f56e864
Ida Tomine Berg <idatbe@ntnu.no>
2023-11-01 17:11:25 +0100

    Removed Note parameter
    - Removed because the abstract controller does not have it
    
    Co-authored-by: Olav Larsen Halleraker <olavlha@stud.ntnu.no>

--- notes/ui/src/main/java/ui/controllers/CreateUserController.java
+++ notes/ui/src/main/java/ui/controllers/CreateUserController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import core.UserValidation;
+import dataaccess.LocalNotesAccess;
 import core.NoteOverview;
 import core.User;
 
@@ -15,16 +16,18 @@ import core.User;
  * This is the class for controlling CreateUser scene.
  */
 
-public class CreateUserController extends AbstractController{
-    
-    @FXML 
+public class CreateUserController extends AbstractController {
+
+    @FXML
     private TextField createUsernameInput;
-    @FXML 
+    @FXML
     private TextField createPasswordInput;
-    @FXML 
+    @FXML
     private TextField confirmPasswordInput;
-    @FXML private Button createUser;
-    @FXML private Text errorMessageDisplay;
+    @FXML
+    private Button createUser;
+    @FXML
+    private Text errorMessageDisplay;
 
     private String username;
     private String password;
@@ -32,21 +35,20 @@ public class CreateUserController extends AbstractController{
 
     @FXML
     private void createUser(ActionEvent event) throws IOException {
-        
+
         try {
-            UserValidation.checkValidUsername(createUsernameInput.getText()); 
+            UserValidation.checkValidUsername(createUsernameInput.getText());
             username = createUsernameInput.getText();
-            UserValidation.checkValidPassword(createPasswordInput.getText()); 
+            UserValidation.checkValidPassword(createPasswordInput.getText());
             password = createPasswordInput.getText();
             confirmPassword = confirmPasswordInput.getText().trim();
-            UserValidation.checkEqualPassword(password, confirmPassword); 
-            
-            NoteOverview noteOverview = new NoteOverview(); //empty noteoverview
-            User user = new User(username, password, noteOverview);
+            UserValidation.checkEqualPassword(password, confirmPassword);
 
+            NoteOverview noteOverview = new NoteOverview(); // empty noteoverview
+            User user = new User(username, password, noteOverview);
             getDataAccess().createUser(user);
 
-            setScene(Controllers.LOGIN, event, getDataAccess(), null);
+            setScene(Controllers.LOGIN, event, getDataAccess());
 
         } catch (IllegalArgumentException e) {
             errorMessageDisplay.setText(e.getMessage());
