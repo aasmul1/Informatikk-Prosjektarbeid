36037cdf1564b4d7d3a3ea5b1e9f0dfe6054d996
Hedda Jendem <heddajen@ntnu.no>
2023-11-02 11:52:55 +0100

    Implement UI Testing Methods in LoginControllerTest
    - start(Stage stage) method
    - initFields() method

--- notes/ui/src/test/java/ui/controllers/LoginControllerTest.java
+++ notes/ui/src/test/java/ui/controllers/LoginControllerTest.java
@@ -1,7 +1,54 @@
 package ui.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 
-public class LoginControllerTest extends ApplicationTest{
+import dataaccess.LocalNotesAccess;
+import dataaccess.NotesAccess;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import ui.App;
+
+public class LoginControllerTest extends ApplicationTest {
+
+    private TextField usernameField;
+    private TextField passwordField;
+    private Button logInButton;
+    private NotesAccess dataAccess = new LocalNotesAccess();
+    private TextArea errorMessage;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(App.class.getResource("Login.fxml"));
+
+        LoginController controller = new LoginController();
+        fxmlLoader.setController(controller);
+
+        controller.setDataAccess(dataAccess);
+    
+        final Parent parent = fxmlLoader.load();
+        stage.setScene(new Scene(parent));
+        stage.show();
+    }
+
+    @BeforeEach
+    public void initFields() {
+        usernameField = lookup("#usernameInput").query();
+        passwordField = lookup("#passwordInput").query();
+        logInButton = lookup("#loginButton").query();
+        errorMessage = lookup("#errorMessage").query();
+    }
+
     
 }
