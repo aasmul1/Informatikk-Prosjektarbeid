f41e0a4767d0a3b400a29e9613742ec4fa212c03
olavhalleraker <ohallera@gmail.com>
2023-11-04 11:44:52 +0100

    Added neccessary methods in NotesService
    - Preperation for creating the controller
    - Methods created:
    - manuallyCreateAccounts for temp. setup for testing
    - validLogin for authenticating login information
    - getUserByUsername for easy use in HTTP requests
    - getNoteOverviewByUsername with same explenation as above
    - addNote to add new notes
    - createUser to create new user
    - deleteNote to delete notes
    - sort methods

--- notes/rest/src/main/java/rest/NotesService.java
+++ notes/rest/src/main/java/rest/NotesService.java
@@ -1,76 +1,124 @@
 package rest;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.core.exc.StreamWriteException;
-import com.fasterxml.jackson.databind.DatabindException;
 
 import core.Accounts;
 import core.Note;
 import core.NoteOverview;
 import core.User;
 import json.AccountsPersistence;
+import rest.exceptions.FileException;
+import rest.exceptions.UserNotFoundException;
 
 @Service
 public class NotesService {
 
     private Accounts accounts;
     private static final AccountsPersistence PERSISTENCE = new AccountsPersistence();
-    private Path fileLocation;
-
+    // private Path fileLocation;
 
     public NotesService(Accounts accounts) {
+        // TODO: get saved users in json
         this.accounts = accounts;
         PERSISTENCE.setFilePath("springbootserver-notes.json");
-        try {
-            Accounts acc = new Accounts();
-            accounts = acc;
-            acc.addUser(new User("olav", "passord", new NoteOverview()));
-            PERSISTENCE.saveAccounts(acc);
-            accounts = PERSISTENCE.loadAccounts();
-        } catch (StreamWriteException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (DatabindException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-      }
-      
-      @Autowired
-      public NotesService() {
-          this(manuallyCreateAccounts());
-      }
+    }
+
+    @Autowired
+    public NotesService() {
+        this(manuallyCreateAccounts());
+        save();
+    }
+
+    public AccountsPersistence getPersistence() {
+        return PERSISTENCE;
+    }
 
     private static Accounts manuallyCreateAccounts() {
         Accounts acc = new Accounts();
+        // For testing
         NoteOverview noteOverview = new NoteOverview();
+        NoteOverview noteOverview2 = new NoteOverview();
         noteOverview.addNote(new Note("title", "text"));
-        acc.addUser(new User("olav", "passord1", noteOverview));
+        noteOverview2.addNote(new Note("title", "text"));
+        noteOverview.addNote(new Note("title2", "text2"));
+        noteOverview2.addNote(new Note("title2", "text2"));
+        acc.addUser(new User("userone", "Password1", noteOverview));
+        acc.addUser(new User("usertwo", "Password2", noteOverview2));
         return acc;
-      }
-
+    }
 
     public Accounts getAccounts() {
         return accounts;
     }
-    
-    public boolean userLogin(String username, String password) {
+
+    public boolean validLogin(String username, String password) {
         return accounts.checkValidUserLogin(username, password);
     }
 
     public User getUserByUsername(String username) {
+        if (accounts.getUser(username) == null) {
+            throw new UserNotFoundException();
+        }
         return accounts.getUser(username);
     }
 
+    public NoteOverview getNoteOverviewByUsername(String username) {
+        if (accounts.getUser(username) == null) {
+            throw new UserNotFoundException();
+        }
+        return accounts.getUser(username).getNoteOverview();
+    }
 
-    
+    protected void save() {
+        try {
+            PERSISTENCE.saveAccounts(accounts);
+        } catch (IOException e) {
+            throw new FileException();
+        }
+    }
+
+    public Note updateNote(Note note) {
+        save();
+        return note;
+    }
+
+    public void addNote(String username, Note note) {
+        getUserByUsername(username).addNote(note);
+        save();
+    }
+
+    public void createUser(User user) {
+        accounts.addUser(user);
+        save();
+    }
+
+    public NoteOverview getUserNoteOverview(String username) {
+        return getUserByUsername(username).getNoteOverview();
+    }
+
+    public void deleteNote(String username, int index) {
+        getUserByUsername(username).getNoteOverview().deleteNote(index);
+        save();
+    }
+
+    public User getUser(String username, String password) {
+        return accounts.getUser(username, password);
+    }
+
+    public void sortNotesByCreatedDate(String username) {
+        getNoteOverviewByUsername(username).sortNotesByCreatedDate();
+        save();
+    }
+
+    public void sortNotesByLastEditedDate(String username) {
+        getNoteOverviewByUsername(username).sortNotesByLastEditedDate();
+        save();
+    }
+
+    public void sortNotesByTitle(String username) {
+        getNoteOverviewByUsername(username).sortNotesByTitle();
+        save();
+    }
 }
