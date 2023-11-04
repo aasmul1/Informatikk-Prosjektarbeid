4aa6db92e943d6fa1c808a037edaf267d84adad5
aasmul <aasmul@stud.ntno.no>
2023-11-04 12:13:40 +0100

    Added deleteNote and editNote test. Added imports

--- notes/ui/src/test/java/ui/controllers/AppControllerTest.java
+++ notes/ui/src/test/java/ui/controllers/AppControllerTest.java
@@ -1,21 +1,28 @@
 package ui.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationTest;
 
+
+import core.Errors;
 import dataaccess.LocalNotesAccess;
 import dataaccess.NotesAccess;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import ui.App;
@@ -28,7 +35,8 @@ public class AppControllerTest extends ApplicationTest{
     private ListView<String> listView;
     private Text errorMessage;
     private ComboBox<String> comboBox;
-
+    
+    private FxRobot robot = new FxRobot();
     private NotesAccess dataAccess = new LocalNotesAccess();
     
     @Override
@@ -70,7 +78,34 @@ public class AppControllerTest extends ApplicationTest{
 
     @Test
     public void deleteNote(){
-        
+        robot.clickOn("#DeleteNoteButton");
+        assertEquals(Errors.SELECT_NOTE.getMessage(), errorMessage.getText());
+        robot.clickOn("#NewNoteButton");        
+        robot.clickOn("#newNoteInputTitle").write("New Title");
+        robot.clickOn("#newNoteInputText").write("Text two");
+        robot.clickOn("#saveNoteButton");
+
+        Node node = listView.lookup(".thumb");
+        robot.clickOn(node, MouseButton.PRIMARY);
+        robot.clickOn("#DeleteNoteNutton");
+        assertTrue(errorMessage.getText().isEmpty());
+
+    }
+
+    @Test
+    public void editNote(){
+        robot.clickOn("#editNoteButton");
+        assertEquals(Errors.SELECT_NOTE.getMessage(), errorMessage.getText());
+        robot.clickOn("#NewNoteButton");        
+        robot.clickOn("#newNoteInputTitle").write("Another Title");
+        robot.clickOn("#newNoteInputText").write("Text again");
+        robot.clickOn("#saveNoteButton");
+
+        Node node = listView.lookup(".thumb");
+        robot.clickOn(node, MouseButton.PRIMARY);
+        robot.clickOn("#editNoteNutton");
+        assertTrue(errorMessage.getText().isEmpty());
+
     }
 
 }
