9d4679bc372eeb0d670136ce47198bf5e49c2bf4
olavhalleraker <ohallera@gmail.com>
2023-11-04 11:41:17 +0100

    RestServerApplication cleanup
    - Removed uneccessary comments

--- notes/rest/src/main/java/rest/RestServerApplication.java
+++ notes/rest/src/main/java/rest/RestServerApplication.java
@@ -2,36 +2,32 @@ package rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Contains the starting method for the server application.
- * Configures Springs ObjectMapper for JSON to parse classes from the core-module.
+ * Configures Springs ObjectMapper for JSON to parse classes from the
+ * core-module.
  */
 @SpringBootApplication
-// @EnableConfigurationProperties({
-//     FileStorageProperties.class
-// })
 public class RestServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestServerApplication.class, args);
-      }
-    
+    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-      return new WebMvcConfigurer() {
-        @Override
-          public void addCorsMappings(CorsRegistry registry) {
-          registry
-                  .addMapping("/notes/**")
-                  .allowedOrigins("http://localhost:3000")
-                  .allowedMethods("PUT", "POST", "GET");
-        }
-      };
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/notes/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("PUT", "POST", "GET");
+            }
+        };
     }
 
 }
