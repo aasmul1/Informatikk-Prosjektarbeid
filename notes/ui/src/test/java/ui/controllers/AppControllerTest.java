e7c9fb573c72762c5f1264aaff3b7d48b41fc91e
aasmul <aasmul@stud.ntno.no>
2023-11-04 11:42:19 +0100

    Added neccessary imports and added start method - Declaired fields

--- notes/ui/src/test/java/ui/controllers/AppControllerTest.java
+++ notes/ui/src/test/java/ui/controllers/AppControllerTest.java
@@ -1,7 +1,42 @@
 package ui.controllers;
 
+import java.io.IOException;
+
 import org.testfx.framework.junit5.ApplicationTest;
 
+import dataaccess.LocalNotesAccess;
+import dataaccess.NotesAccess;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import ui.App;
+
 public class AppControllerTest extends ApplicationTest{
+
+    private Button deleteButton;
+    private Button editButton;
+    private Button newNoteButton;
+    private ListView<String> listView;
+    private Text errorMessage;
+
+    private NotesAccess dataAccess = new LocalNotesAccess();
     
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(App.class.getResource("App.fxml"));
+
+        LoginController controller = new LoginController();
+        fxmlLoader.setController(controller);
+
+        controller.setDataAccess(dataAccess);
+
+        final Parent parent = fxmlLoader.load();
+        stage.setScene(new Scene(parent));
+        stage.show();
+    }
 }
