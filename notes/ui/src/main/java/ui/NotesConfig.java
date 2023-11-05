dd688de536e082e3bcda7047aafa00cc8b980c84
olavhalleraker <ohallera@gmail.com>
2023-11-05 16:59:36 +0100

    NotesConfig and properties
    Added NotesConfig class and notes.properties to easily change URI.
    More config props TBA.

---
+++ notes/ui/src/main/java/ui/NotesConfig.java
@@ -0,0 +1,28 @@
+package ui;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class NotesConfig {
+    private Properties properties;
+
+    public NotesConfig() {
+        this.properties = new Properties();
+
+        try {
+            properties.load(App.class.getResourceAsStream("notes.properties"));
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not load salarychecker.properties");
+        }
+    }
+
+    /**
+     * With a key, value from notes.properties will be returned
+     *
+     * @param key the key
+     * @return the value
+     */
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
