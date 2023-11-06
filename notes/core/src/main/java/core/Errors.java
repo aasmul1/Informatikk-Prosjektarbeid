9dcb852c2e96caf20d9b7d7d14121f23fc9fde61
Ida Tomine Berg <idatbe@ntnu.no>
2023-11-06 21:05:22 +0100

    Changed typos in error messages

--- notes/core/src/main/java/core/Errors.java
+++ notes/core/src/main/java/core/Errors.java
@@ -1,13 +1,13 @@
 package core;
 
 public enum Errors {
-  USERNAME_FIELD_EMPTY("Please enter an username."),
+  USERNAME_FIELD_EMPTY("Please enter a username."),
   PWD_FIELD_EMPTY("Please enter a password."),
   EVERYTHING_EMPTY("All fields must be filled out."),
   INVALID_USERNAME("Name should only contain letters, and be atleast five letters.."),
   INVALID_PWD("Invalid password, must be at least 8 characters"
     + " and contain at least 1 digit and 1 lower and uppercase letter."),
-  INVALID_USERNAME_AND_OR_PWD("Invalid username address or password."),
+  WRONG_PASSWORD("Wrong password"),
   NOT_REGISTERED("This user is not registered."),
   NOT_EQUAL_PASSWORD("Passwords does not match."),
   NOT_EQUAL_USERNAME("Username do not match"),
