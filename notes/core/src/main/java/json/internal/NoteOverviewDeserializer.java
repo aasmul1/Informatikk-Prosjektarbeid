860ad200813fb268be00972600e0ed383ce86ed1
aasmul <aasmul@stud.ntno.no>
2023-11-07 09:21:51 +0100

    Removed unused import

--- notes/core/src/main/java/json/internal/NoteOverviewDeserializer.java
+++ notes/core/src/main/java/json/internal/NoteOverviewDeserializer.java
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import core.Note;
 import core.NoteOverview;
