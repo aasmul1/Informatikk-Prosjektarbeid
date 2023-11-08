cfe246974189f5aeed4d4f2cd0c3da6b63ade49c
aasmul <aasmul@stud.ntno.no>
2023-11-08 11:57:54 +0100

    Removed and added imports
    - Added method to test the headless method
    -Also added tearDown method after each test

--- notes/ui/src/test/java/ui/AppTest.java
+++ notes/ui/src/test/java/ui/AppTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.framework.junit5.Start;
 
-import static org.testfx.assertions.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
 
 public class AppTest extends ApplicationTest {
 
@@ -23,5 +26,32 @@ public class AppTest extends ApplicationTest {
         assertThat(lookup("#errorMessage").tryQuery()).isPresent();
 
 
+    }
+
+    @Test
+    void testSupportHeadlessWhenHeadlessIsTrue() {
+        // Arrange
+        System.setProperty("headless", "true");
+
+        // Act
+        App.supportHeadless();
+
+        // Assert
+        assertEquals("glass", System.getProperty("testfx.robot"));
+        assertEquals("true", System.getProperty("testfx.headless"));
+        assertEquals("sw", System.getProperty("prism.order"));
+        assertEquals("t2k", System.getProperty("prism.text"));
+        assertEquals("true", System.getProperty("java.awt.headless"));
+
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        System.clearProperty("testfx.robot");
+        System.clearProperty("testfx.headless");
+        System.clearProperty("prism.order");
+        System.clearProperty("prism.text");
+        // Be cautious about clearing "java.awt.headless" as it might affect other parts of the test suite.
     }
 }
